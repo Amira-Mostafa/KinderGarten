6788b3278a2fa7102aba14f06501902b0a55ee05
Amira-Mostafa <amirazalazel@gmail.com>
2024-04-14 03:06:24 +0200

    build:Orgainzing files

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+    @extends('layouts.template')
+
+    @section('title')
+        Home Page
+    @endsection
+
+
+    @section('content')
+
+        @include('includes.Spinner')
+        @include('includes.navBar')
+        @include('includes.Carousel')
+        @include('includes.Facilities')
+        @include('includes.about')
+        @include('includes.callToAction')
+        @include('includes.Classes')
+        @include('includes.Appointment')
+        @include('includes.Team')
+        @include('includes.Testimonial')
+
+    @endsection
+
+       
+
